16577cdc4240d71e0ba9314779bff3c3ab56b801
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-10-08 12:58:27 +0300

    populating data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Movies from "./components/movies";
 
 function App() {
   return (
     <main className="container">
-      <h1>Hello World! </h1>
+      <Movies />
     </main>
   );
 }
