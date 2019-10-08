bdff6aec87b8d2eb6d846df99465813a7e0f1fea
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-10-08 12:32:25 +0300

    vidly-started

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Hello World! </h1>
+    </main>
+  );
+}
+
+export default App;
