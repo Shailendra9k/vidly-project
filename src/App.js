eb31468a1d2498d0b2caa6fa89baae4d2dc416bb
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-11-28 06:39:23 +0200

    loging from initiated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Customers from "./components/customer";
 import NotFound from "./components/notFound";
 import Rentals from "./components/rentals";
 import NavBar from "./components/navBar";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const username = this.username.current.value;
+    console.log("submitted");
+  };
+
+  handleChange = e => {
+    const account = { ...this.state.account };
+    account[e.currentTarget.name] = e.currentTarget.value;
+    this.setState({ account });
+  };
+
+  render() {
+    const { account } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              id="username"
+              name="username"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              type="text"
+              name="password"
+              className="form-control"
+            />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
