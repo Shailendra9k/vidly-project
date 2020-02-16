ceeec136d5cc2067d0daceb345a5ce07a3c8ef5e
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-02-17 01:29:29 +0200

    validing fields

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -26,14 +26,28 @@ class LoginForm extends Component {
     const errors = this.validate();
     this.setState({ errors: errors || {} });
     if (errors) return;
+
     //Call the server
     console.log("submitted");
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+    }
+  };
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
